b0bb11042cb5e493b4e0e3be70a46156a9e11ff9
David Lehman <dlehman@redhat.com>
2015-04-21 11:19:53 -0500

    Only return target size for resizable devices if it has been set.

--- blivet/devices/storage.py
+++ blivet/devices/storage.py
@@ -486,7 +486,7 @@ class StorageDevice(Device):
     def _getSize(self):
         """ Get the device's size, accounting for pending changes. """
         size = self._size
-        if self.exists and self.resizable:
+        if self.exists and self.resizable and self.targetSize:
             size = self.targetSize
 
         return size
